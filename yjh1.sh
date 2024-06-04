57ad0dc6598c3f3c74f5a7f29c7ffc2f7aec882d
gyx131 <119396411+gyx131@users.noreply.github.com>
2024-06-04 11:11:58 +0800

    Update yjh1.sh

--- yjh1.sh
+++ yjh1.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# 检查内核版本
+kernel_version=$(uname -r | cut -d '.' -f1-2)
+if [[ "$kernel_version" < "4.9" ]]; then
+    echo "Kernel version is lower than 4.9. Installing BBR patch..."
+    yum install wget
+    wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh
+else
+    echo "Kernel version is 4.9 or higher. Applying BBR settings directly..."
+    echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+    echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+    sysctl -p
+fi
+
+# 检查BBR是否启用
+echo "Checking if BBR is enabled..."
+lsmod | grep bbr
+sysctl net.ipv4.tcp_available_congestion_control
+
 # 检查firewalld状态
 echo "Firewalld Status:"
 systemctl status firewalld.service
@@ -8,17 +26,6 @@ systemctl status firewalld.service
 echo "Stopping and disabling firewalld..."
 systemctl stop firewalld.service
 systemctl disable firewalld.service
-
-# 配置BBR拥塞控制
-echo "Enabling BBR congestion control..."
-echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-sysctl -p
-
-# 检查BBR是否启用
-echo "Checking if BBR is enabled..."
-lsmod | grep bbr
-sysctl net.ipv4.tcp_available_congestion_control
 
 # 安装lrzsz
 echo "Installing lrzsz..."
